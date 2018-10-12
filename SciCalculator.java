4b8a66dba4958b81dda6b305bc8f037d5935908f
Sam Shulman <samshulman@Sams-MacBook-Air.local>
2018-10-12 10:10:08 -0400

    first commit

---
+++ SciCalculator.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class SciCalculator {
+
+    public static void main(String[] args) {
+        Scanner operation = new Scanner(System.in);
+        {
+            //Defining Variables
+            int operater = 0;
+            double x;
+            int count = 0;
+            double y;
+            int operator;
+            double answer = 0;
+            double number = 0;
+            boolean exit = false;
+            double average = 0;
+            boolean jump = true;
+            System.out.print("Current Result
